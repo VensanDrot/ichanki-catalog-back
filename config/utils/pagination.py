317e536581c8ba5f48147d42febab6ed5b8a5a9b
abd-Kabir <kabirumarov@gmail.com>
2024-04-15 18:00:58 +0500

    file changes

--- config/utils/pagination.py
+++ config/utils/pagination.py
@@ -9,36 +9,6 @@ class APIPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 100
 
-    # def paginate_queryset(self, queryset, request, view=None):
-    #     # total_pages = len(queryset) // self.get_page_size(request) + 1
-    #     # req_page = request.query_params.get('page')
-    #     # if req_page:
-    #     #     if int(req_page) >= total_pages:
-    #     #         return []
-    #     #     else:
-    #     return super().paginate_queryset(queryset, request, view)
-    #
-    # def get_paginated_response(self, data):
-    #     if data:
-    #         return Response({
-    #             'links': {
-    #                 'next': self.get_next_link(),
-    #                 'previous': self.get_previous_link()
-    #             },
-    #             'count': self.page.paginator.count,
-    #             'total_pages': self.page.paginator.num_pages,
-    #             'results': data
-    #         })
-    #     else:
-    #         return Response({
-    #             'links': {
-    #                 'next': None,
-    #                 'previous': None
-    #             },
-    #             'count': 0,
-    #             'total_pages': 0,
-    #             'results': None
-    #         })
     def get_paginated_response(self, data):
         return Response({
             'count': self.page.paginator.count,
